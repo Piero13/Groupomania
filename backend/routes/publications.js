b420d2e41b2a12abdae1c7e9d20dbe06c1312272
Pierre Fasce <lemonpiero@gmail.com>
2022-03-13 15:46:58 +0100

    intégration des routes comments aux routes publications, suppression du fichier routes comments

--- backend/routes/publications.js
+++ backend/routes/publications.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const publicationsCtrl = require('../controllers/publications')
+const commentsCtrl = require('../controllers/comments')
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
@@ -13,5 +14,12 @@ router.get('/all', auth, publicationsCtrl.getAllPublications);
 router.get('/:id', auth, publicationsCtrl.getPublication);
 router.post('/:id/like', auth, publicationsCtrl.likes);
 router.get('/:id/likes', auth, publicationsCtrl.getLikes);
+
+// Routes commentaires
+router.post('/:publicationId/comments', auth, commentsCtrl.createComment);
+router.put('/:publicationId/comments/:id', auth, commentsCtrl.updateComment);
+router.delete('/:publicationId/comments/:id', auth, commentsCtrl.deleteComment);
+router.get('/:publicationId/comments', auth, commentsCtrl.getAllComments);
+router.get('/:publicationId/comments/:id', auth, commentsCtrl.getComment);
 
 module.exports = router;
